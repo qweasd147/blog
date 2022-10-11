526b8e797000855f192567cbbc401675b334837b
joohyung <qweasd_147@naver.com>
2022-10-11 18:01:15 +0000

    updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d8ed385cb558efbb7e56.js"
+    "url": "webpack-runtime-f551f645da021aea5c45.js"
   },
   {
     "url": "styles.0ad89aece188ac638a5b.css"
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "framework-5aab6d6db7fbdb045164.js"
   },
   {
-    "url": "app-5ac963d69baf2724a083.js"
+    "url": "app-ffde870ffe44c69e5220.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "258a41871748c533c800bac4d65d555d"
+    "revision": "1326b05809729ee8b3e9d67f20b51711"
   },
   {
     "url": "polyfill-4f8c4c04526091b1ccaa.js"
@@ -157,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/blog/app-5ac963d69baf2724a083.js`))) {
+  if (!resources || !(await caches.match(`/blog/app-ffde870ffe44c69e5220.js`))) {
     return await fetch(event.request)
   }
 
