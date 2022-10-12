d1f4a419e68af8377092f775272993870cee6b89
joohyung <qweasd_147@naver.com>
2022-10-12 21:33:07 +0900

    state 없을때 에러 발생 안하도록 수정

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -5,7 +5,7 @@ import "./src/assets/scss/main.scss";
 export const shouldUpdateScroll = ({
   routerProps,
 }: ShouldUpdateScrollArgs): boolean => {
-  const { shouldUpdateScroll: shouldUpdate } = routerProps.location.state;
+  const { shouldUpdateScroll: shouldUpdate } = routerProps.location.state || {};
 
   return shouldUpdate;
 };
